768fda2697e43eb11a03e6eb639780784b305d8a
PerduGames <perdugames@gmail.com>
2017-06-13 07:06:36 -0300

    Create OffTags.cs

--- OffTags.cs
+++ OffTags.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 
-public class limparTags : MonoBehaviour {
+public class OffTags : MonoBehaviour {
 
 	//////////////////////////////////////
 	//Script by PerguGames
